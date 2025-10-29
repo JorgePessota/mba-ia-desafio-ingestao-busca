3c057f8972e4db49cded8e29ecc27a21af3fd399
Jorge Henrique Pessota <jorgepessota@MacBook-Air.local>
2025-10-29 00:50:24 -0300

    feat: implement PDF ingestion with embeddings; update docker-compose and requirements

--- src/ingest.py
+++ src/ingest.py
@@ -1,4 +1,8 @@
 import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,7 +10,20 @@ load_dotenv()
 PDF_PATH = os.getenv("PDF_PATH")
 
 def ingest_pdf():
-    pass
+    loader = PyPDFLoader(PDF_PATH)
+    docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    chunks = splitter.split_documents(docs)
+    
+    embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL"))
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+    )
+
+    store.add_documents(documents=chunks)
 
 
 if __name__ == "__main__":
