d8abe17418133a0c1ecaeeb08e08d08f9ec41445
Jorge Henrique Pessota <jorgepessota@MacBook-Air.local>
2025-11-02 01:42:31 -0300

    feat: implement search.py with embeddings and chat.py

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,26 @@
 from search import search_prompt
 
 def main():
-    chain = search_prompt()
+    while True:
+        try:
+            user_input = input("Faça sua pergunta (digite \"sair\" para encerrar): ")
 
-    if not chain:
-        print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
-        return
+            if user_input.lower() in ["sair"]:
+                print("Até logo!")
+                break 
+          
+            chain = search_prompt(user_input)
+
+            if not chain:
+                print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
+                return 
+            else:
+                print(chain) 
+
+        except Exception as e:
+            print(f"Ocorreu um erro {e}") 
+
     
-    pass
 
 if __name__ == "__main__":
     main()
